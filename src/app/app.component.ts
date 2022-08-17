90d61417af282c35f12f08c35f53a40a43034fc4
Marcos Felipe <p.marcosfelipe@gmail.com>
2022-08-17 23:21:49 +0100

    create remove function and use binding to trigger the event  when button ]pressed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,26 @@ import { Todo } from 'src/models/todo.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   public todos: Todo[] = [];
   public title: String = 'TODOS';
-  
+
   constructor() {
     this.todos.push(new Todo(1, 'Todo 1', false));
     this.todos.push(new Todo(2, 'Todo 2', false));
     this.todos.push(new Todo(3, 'Todo 3', true));
   }
 
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
 
+  markAsDone(todo: Todo) {}
+
+  markAsUndone(todo: Todo) {}
 }
